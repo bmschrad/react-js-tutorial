c6553792bb4697da3d940030f7d91ca047249738
Brandon <bmschrad@hotmail.com>
2018-03-25 18:55:15 -0500

    End of 2nd video

---
+++ src/js/client.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+    constructor() {
+        super();
+        this.name = "Brandon"
+    }
+
+    render() {
+        return (
+            <h1>It's {this.name}!</h1>
+        );
+    }
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
