cc53257de23ac798a163950e953973e196bb90c4
Brandon <bmschrad@hotmail.com>
2018-03-29 21:15:02 -0500

    Done with 7 but need to breakout info

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -7,11 +7,13 @@ export default class Layout extends React.Component {
     }
 
     render() {
+        const { history } = this.props;
+        console.log(history.isActive("archives"));
         return (
             <div>
                 {this.props.children}
                 <h1>KillerNews.net</h1>
-                <Link to="archives" class="btn btn-danger">archives</Link>
+                <Link to="archives" activeClassName="test" class="btn btn-danger">archives</Link>
                 <Link to="settings" class="btn btn-success">settings</Link>
                 <button onClick={this.navigate.bind(this)}>featured</button>
             </div>
