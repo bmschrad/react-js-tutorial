0bf07ff86ba691938819414f784e51ef54a9dded
Brandon <bmschrad@hotmail.com>
2018-04-01 20:03:24 -0500

    Added a few more files for video 8

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Link } from "react-router";
 
-export default class Layout extends React.Component {
-    navigate(){
-        this.props.history.pushState(null,"/");
-    }
+import Footer from "../compnents/layout/Footer";
+import Nav from "../compnents/layout/Nav";
 
+export default class Layout extends React.Component {
     render() {
-        const { history } = this.props;
-        console.log(history.isActive("archives"));
+        const { location } = this.props;
+        const containerSyle = {
+            marginTop: "60px"
+        };
         return (
             <div>
-                {this.props.children}
-                <h1>KillerNews.net</h1>
-                <Link to="archives" activeClassName="test" class="btn btn-danger">archives</Link>
-                <Link to="settings" class="btn btn-success">settings</Link>
-                <button onClick={this.navigate.bind(this)}>featured</button>
+                <Nav location={location} />
+
+                <div class ="container" style={containerSyle}>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1>KillerNews.net</h1>
+                            {this.props.children}
+                        </div>        
+                    </div>        
+                <Footer />
+                </div>
             </div>
         );
     }
